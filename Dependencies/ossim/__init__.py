464b082dea311a3c841aae715ae2dca8659f60c5
Trevor R.H. Clarke <tclarke@ball.com>
2009-02-03 17:57:56 +0000

    OPTICKS-443: Allow import of NITFs with ICHIPB rotation

--- Dependencies/ossim/__init__.py
+++ Dependencies/ossim/__init__.py
@@ -1,3 +1,3 @@
 __doc__ = """OSSIM library."""
-package = {'url':"https://opticks.ballforge.net/files/documents/8/74/ossim.zip",
-           'downloadLocation':'ossim-10682-Nov28-2007.zip'}
+package = {'url':"https://opticks.ballforge.net/files/documents/8/227/ossim-10682-Jan26-2009.zip",
+           'downloadLocation':'ossim-10682-Jan26-2009.zip'}
