ae5366c6aa190817358502600d5ecc6f61a3e03e
Dustan Adkins <dadkins@ball.com>
2009-05-13 16:47:18 +0000

    OPTICKS-595: NITF IMODE B Multi-Blocked files fail to load

--- Dependencies/ossim/__init__.py
+++ Dependencies/ossim/__init__.py
@@ -1,3 +1,3 @@
 __doc__ = """OSSIM library."""
-package = {'url':"https://opticks.ballforge.net/files/documents/8/256/ossim-10682-Apr16-2009.zip",
-           'downloadLocation':'ossim-10682-Apr16-2009.zip'}
+package = {'url':"https://opticks.ballforge.net/files/documents/8/279/ossim-10682-May13-2009.zip",
+           'downloadLocation':'ossim-10682-May13-2009.zip'}
