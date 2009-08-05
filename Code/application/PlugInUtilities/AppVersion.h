9cde2d681fc2389b17004a414ddf85e71701b59f
Kip Streithorst <kstreith@ball.com>
2009-08-05 21:20:54 +0000

    ReleaseManager021: Updated version # and release date for 4.3.1rc1.

--- Code/application/PlugInUtilities/AppVersion.h
+++ Code/application/PlugInUtilities/AppVersion.h
@@ -14,11 +14,11 @@
 #define APP_NAME_LONG "Opticks"
 #define APP_COPYRIGHT "Copyright © 2008, Ball Aerospace & Technologies Corp."
 #define APP_SPONSOR "Ball"
-#define APP_VERSION_NUMBER "4.3.1Unofficial"
+#define APP_VERSION_NUMBER "4.3.1rc1"
 #define APP_IS_PRODUCTION_RELEASE false
 #define APP_COPYRIGHT_MSG "© 2000 - 2008 Ball Aerospace & Technologies Corp., Fairborn, Ohio"
 #define APP_RELEASE_DATE_YEAR 2009
-#define APP_RELEASE_DATE_MONTH 5
-#define APP_RELEASE_DATE_DAY 22
+#define APP_RELEASE_DATE_MONTH 8
+#define APP_RELEASE_DATE_DAY 7
 
 #endif
