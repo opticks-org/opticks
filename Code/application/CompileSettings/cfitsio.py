5d26213ed9d60c71cd32f4e05b5f64a749ba191b
Trevor R.H. Clarke <tclarke@ball.com>
2009-12-31 15:00:45 +0000

    OPTICKS-828:Add an importer for FITS data

---
+++ Code/application/CompileSettings/cfitsio.py
@@ -0,0 +1,21 @@
+import os
+import os.path
+import SCons.Warnings
+
+class CfitsioNotFound(SCons.Warnings.Warning):
+    pass
+SCons.Warnings.enableWarningClass(CfitsioNotFound)
+
+def generate(env):
+    path = os.path.join(os.environ.get('OPTICKSDEPENDENCIES',None))
+    if path:
+       path = os.path.join(path, "cfitsio")
+    if not path:
+       SCons.Warnings.warn(CfitsioNotFound,"Could not detect cfitsio")
+    else:
+       env.AppendUnique(CXXFLAGS=["-I%s/include" % (path,)],
+                        LIBPATH=['%s/lib/%s' % (path,env["PLATFORM"])],
+                        LIBS=["cfitsio"])
+
+def exists(env):
+    return env.Detect('cfitsio')
