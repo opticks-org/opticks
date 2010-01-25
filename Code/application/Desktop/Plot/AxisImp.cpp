10798a23515a26c5b8cf90a5601fe9dccc146ae8
David Sulgrove <dsulgrov@ball.com>
2010-01-25 20:47:13 +0000

    OPTICKS-842: Plot axis does not restore correctly from a saved session

--- Code/application/Desktop/Plot/AxisImp.cpp
+++ Code/application/Desktop/Plot/AxisImp.cpp
@@ -496,6 +496,8 @@ bool AxisImp::toXml(XMLWriter* pXml) const
    pXml->addAttr("maxMajorTicks", mMaxMajorTicks);
    pXml->addAttr("maxMinorTicks", mMaxMinorTicks);
    pXml->addAttr("scaleDrawLabelFormat", mScaleDraw.getLabelFormat().toStdString());
+   pXml->addAttr("minValue", getMinimumValue());
+   pXml->addAttr("maxValue", getMaximumValue());
    return true;
 }
 
@@ -526,6 +528,10 @@ bool AxisImp::fromXml(DOMNode* pDocument, unsigned int version)
    mMaxMajorTicks = StringUtilities::fromXmlString<int>(A(pElmnt->getAttribute(X("maxMajorTicks"))));
    mMaxMinorTicks = StringUtilities::fromXmlString<int>(A(pElmnt->getAttribute(X("maxMinorTicks"))));
    mScaleDraw.setLabelFormat(A(pElmnt->getAttribute(X("scaleDrawLabelFormat"))));
+
+   double minValue = StringUtilities::fromXmlString<double>(A(pElmnt->getAttribute(X("minValue"))));
+   double maxValue = StringUtilities::fromXmlString<double>(A(pElmnt->getAttribute(X("maxValue"))));
+   setValueRange(minValue, maxValue);
 
    updateScale();
    updateSize();
