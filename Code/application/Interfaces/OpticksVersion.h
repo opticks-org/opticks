617c689e25bff4edcf44c4d5c1d88aa2188284e7
Michael Considine <mconsidi@ball.com>
2014-07-31 17:58:51 +0000

    Update version number for 4.12.0

--- Code/application/Interfaces/OpticksVersion.h
+++ Code/application/Interfaces/OpticksVersion.h
@@ -10,6 +10,6 @@
 #ifndef OPTICKSVERSION_H
 #define OPTICKSVERSION_H
 
-#define OPTICKS_VERSION "4.12Unofficial"
+#define OPTICKS_VERSION "4.12.0"
 
 #endif
--- Code/application/PlugInUtilities/AppVersion.h
+++ Code/application/PlugInUtilities/AppVersion.h
@@ -14,11 +14,11 @@
 #define APP_NAME_LONG "Opticks"
 #define APP_COPYRIGHT "Copyright © 2008, Ball Aerospace & Technologies Corp."
 #define APP_SPONSOR "Ball"
-#define APP_VERSION_NUMBER "4.12Unofficial"
+#define APP_VERSION_NUMBER "4.12.0"
 #define APP_IS_PRODUCTION_RELEASE false
 #define APP_COPYRIGHT_MSG "© 2000 - 2008 Ball Aerospace & Technologies Corp., Fairborn, Ohio"
-#define APP_RELEASE_DATE_YEAR 2012
-#define APP_RELEASE_DATE_MONTH 9
-#define APP_RELEASE_DATE_DAY 14
+#define APP_RELEASE_DATE_YEAR 2014
+#define APP_RELEASE_DATE_MONTH 8
+#define APP_RELEASE_DATE_DAY 8
 
 #endif
