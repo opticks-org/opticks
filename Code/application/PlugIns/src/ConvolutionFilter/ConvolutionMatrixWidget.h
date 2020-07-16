3e9c8d9a50faf31ec7bf951956b77db1fc2862cb
Trevor R.H. Clarke <trevor@notcows.com>
2020-07-16 09:00:52 -0400

    Update copyright year in all the source files.

--- Code/application/PlugIns/src/ConvolutionFilter/ConvolutionMatrixWidget.h
+++ Code/application/PlugIns/src/ConvolutionFilter/ConvolutionMatrixWidget.h
@@ -1,7 +1,7 @@
 
 /*
  * The information in this file is
- * Copyright(c) 2007 Ball Aerospace & Technologies Corporation
+ * Copyright(c) 2020 Ball Aerospace & Technologies Corporation
  * and is subject to the terms and conditions of the
  * GNU Lesser General Public License Version 2.1
  * The license text is available from   
